6d1748554cb6a3cc4947b5c4dcf8ef6a7205a53d
martindipeco <martindipeco@gmail.com>
2024-04-22 20:18:15 -0300

    get api key en principal

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,8 +1,13 @@
 package principal;
 
+import modelo.ApiKey;
+
 public class Principal {
 
     public static void main(String[] args) {
+
+        String clave = ApiKey.getApiKey();
+
         System.out.println("Bienvenidos a Cambio Alura");
     }
 }
